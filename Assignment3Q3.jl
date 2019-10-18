189c7d8fe17cff2bd80e200ac512d3fa76913539
henrystoldt <hhstoldt@ucalgary.ca>
2019-10-18 08:50:19 -0700

    Q3 Up to desktop

--- Assignment3Q3.jl
+++ Assignment3Q3.jl
@@ -20,33 +20,62 @@ function initT(eta)
 end
 
 #nNodes = number of interior nodes
-function solve_Q3(nNodes)
+function solve_Q3(nNodes, epsilon=0.0001)
     eta1 = 0
     eta2 = 20
     dx = (eta2 - eta1) / (nNodes + 1)
 
     nVars = nNodes * 2
 
+    #Create initial value vectors
+    x = Array{Float64, 1}(undef, nVars)
+    for i in 1:nNodes
+        eta = dx*i
+        x[i] == initZ(eta)
+        x[i+1] == initT(eta)
+    end
+
     # Second order central stencils for each derivative
     cdn3 = (1/(2* dx*dx*dx)) .* [ -1 2 0 -2 1 ]
     cdn2 = (1/(dx*dx)) .* [ 1 -2 1 ]
     cdn = (1/(2*dx)) .* [ -1 0 1 ]
     
-    # Augmented matrix
-    # Order of variables is z1, T1, z2, T2, z3, T3 etc...
-    matrix = Array{Float64, 2}(undef, nVars, nVars+1)
-
     # Build the linear parts of the matrix (first term of both equations)
-    for i in 1:nNodes
-        centerPt = i
-        stencilSemiSpan = floor(size(cdn3) / 2)
-        minIndex = max(1, centerPt - stencilSemiSpan)
-        maxIndex = min(nVars, )
-        for c in range minIndex:
+    # Make a single stencil, replicate it for each row, adjust the first and last for the boundary conditions
+    bandwidth = size(cdn3) * 2
+    
+    # Order of variables is z1, T1, z2, T2, z3, T3 etc...
+    thomasZRow = Array{Float64, 1}(0, bandwidth)
+    for i in 1:size(cdn3)
+        thomasZRow[2*i-1] += cdn3[i]
+    end
+    Zcenter = floor((size(cdn3) + 1) / 2)
+    thomasZRow[Zcenter] += 1
+
+    thomasTRow = Array{Float64, 1}(0, bandwidth)
+    offset = size(cdn3) - size(cdn2)
+    for i in 1+offset:size(cdn2)+offset
+        thomasTRow[2*i] += cdn2[i]
     end
 
+    matrix = Array{Float64, 2}(0, nVars, nVars+1)
+    for r in 1:nNodes
+        matrix[r, 1:nVars] += thomasZRow
+        matrix[r+1, 1:nVars] += thomasTRow
+    end
+        
 
-    
+    while maxDx > epsilon
+        # Create matrix by substituting in nonlinear values from last iteration
+        tZRow = copy(thomasZRow)
+        
+        offset = size(cdn2) - size(cdn3)
+        cdn2_Z2 = 
+        for i in 1+offset:size(cdn2)+offset
+        
+        tTRow = copy(thomasTRow)
+
+    end
 
 
 end
