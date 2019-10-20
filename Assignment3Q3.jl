6fd95a157430cf7c9404244a795b604c89c40058
henrystoldt <hhstoldt@ucalgary.ca>
2019-10-19 23:36:34 -0700

    Switch to desktop

--- Assignment3Q3.jl
+++ Assignment3Q3.jl
@@ -1,12 +1,12 @@
-inlude("LinearSolvers.jl")
+include("LinearSolvers.jl")
 
 # zeta will be referred to as z or Z
 
-function residual1 (d3Zdn, d2Zdn, dZdn, T, Z)
+function residual1(d3Zdn, d2Zdn, dZdn, T, Z)
     return d3Zdn + 3*Z*d2Zdn - 2 *(dZdn*dZdn) + T
 end
 
-function residual2 (d2Tdn, dTdn, Z)
+function residual2(d2Tdn, dTdn, Z)
     return d2Tdn + 3*0.71*Z*dTdn
 end
 
@@ -31,8 +31,8 @@ function solve_Q3(nNodes, epsilon=0.0001)
     x = Array{Float64, 1}(undef, nVars)
     for i in 1:nNodes
         eta = dx*i
-        x[i] == initZ(eta)
-        x[i+1] == initT(eta)
+        x[2*i - 1] == initZ(eta)
+        x[2*i] == initT(eta)
     end
 
     # Second order central stencils for each derivative
@@ -42,40 +42,106 @@ function solve_Q3(nNodes, epsilon=0.0001)
     
     # Build the linear parts of the matrix (first term of both equations)
     # Make a single stencil, replicate it for each row, adjust the first and last for the boundary conditions
-    bandwidth = size(cdn3) * 2
+    bandwidth = size(cdn3, 2) * 2
     
     # Order of variables is z1, T1, z2, T2, z3, T3 etc...
-    thomasZRow = Array{Float64, 1}(0, bandwidth)
-    for i in 1:size(cdn3)
+    # Create template z eqn
+    thomasZRow = zeros(bandwidth)
+    # Add third derivative of zeta
+    for i in 1:size(cdn3, 2)
         thomasZRow[2*i-1] += cdn3[i]
     end
-    Zcenter = floor((size(cdn3) + 1) / 2)
+    # Add Ti
+    Zcenter = floor(Int32, bandwidth / 2)
+    Tcenter = Zcenter + 1
     thomasZRow[Zcenter] += 1
 
-    thomasTRow = Array{Float64, 1}(0, bandwidth)
-    offset = size(cdn3) - size(cdn2)
-    for i in 1+offset:size(cdn2)+offset
-        thomasTRow[2*i] += cdn2[i]
+    println("Bandwidth: $bandwidth")
+    println("Center: $Zcenter")
+
+    println("Template Z Eqn Stencil:")
+    println(thomasZRow)
+
+    # Create template T eqn
+    thomasTRow = zeros(bandwidth)
+    #Add second derivative of T
+    offset = floor(Int32, (size(cdn3, 2) - size(cdn2, 2))/2)
+    for i in 1:size(cdn2,2)
+        thomasTRow[2*(i+offset)] += cdn2[i]
     end
 
-    matrix = Array{Float64, 2}(0, nVars, nVars+1)
+    println("Template T Eqn Stencil:")
+    println(thomasTRow)
+
+    # Create matrix, alternating T and zeta equations
+    matrix = Array{Float64, 2}(undef, nVars, nVars+1)
+    semiSpan = floor(Int32, (bandwidth - 2) / 2)
     for r in 1:nNodes
-        matrix[r, 1:nVars] += thomasZRow
-        matrix[r+1, 1:nVars] += thomasTRow
+        # Copy numbers in to matrix
+        for colIndex in 1:bandwidth
+            column = 2*r - 1 + colIndex - Zcenter
+            if column < 1
+                continue
+            elseif column > nVars
+                break
+            end
+            println("Row: $r, Column: $column")
+            matrix[2*r - 1, column] = thomasZRow[colIndex]
+            matrix[2*r, column] = thomasTRow[colIndex]
+        end
     end
-        
 
+    # Add linear boundary conditions!, matbe set the other augmented terms to zero
+
+    println("Linear terms matrix:")
+    printMatrix(matrix)
+        
+    maxDx = 1
     while maxDx > epsilon
         # Create matrix by substituting in nonlinear values from last iteration
-        tZRow = copy(thomasZRow)
-        
-        offset = size(cdn2) - size(cdn3)
-        cdn2_Z2 = 
-        for i in 1+offset:size(cdn2)+offset
-        
-        tTRow = copy(thomasTRow)
+        matrixI = copy(matrix)
 
+        # Calculate values of nonlinear terms for this iteration, lagging the lowest order terms in each nonlinear term
+        for r in 1:nNodes
+            # Adding second derivative of zeta
+            cdn2_Z2 = cdn2 .* 3 * x[2*r - 1]
+            # Adding squared first derivative of zeta
+            cdn1_Z1 = cdn .* 2 * (x[2*r + 1] - x[2*r - 3]) / (2*dx)
+            semiSpan = floor(Int32, size(cdn2, 2) -1 /2)
+            for a in 1:size(cdn2, 2)
+                offCenter = a - (semiSpan + 1)
+                col = r + offCenter
+                if col < 1
+                    continue
+                elseif col > nVars
+                    break
+                end
+                matrix[2*i-1, col] += cdn2_Z2[a]
+                matrix[2*i-1, col] += cdn1_Z1[a]
+            end
+
+            printMatrix(matrixI)
+
+            #Adding first derivative of T1
+            cdn_T1 = cdn .* 3 * 0.71 * x[2*i]
+            offset = size(cdn, 2) - size(cdn3, 2)
+            for a in 1+offset:size(cdn, 2)+offset
+                matrix[2*i, 2*i] += cdn_T1[a]
+            end
+
+        end
+
+        printMatrix(matrix)
+        # Adjust first and last rows with boundary conditions
+
+
+        # Solve matrix
+
+
+        # Calculate residuals/dxs
+
+        break
     end
-
-
 end
+
+solve_Q3(3)
