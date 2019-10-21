5c85846ab148e3ab92c31109df966248e1c7d4f3
henrystoldt <hhstoldt@ucalgary.ca>
2019-10-21 05:41:42 -0700

    Switch to deskop

--- Assignment3Q3.jl
+++ Assignment3Q3.jl
@@ -40,6 +40,10 @@ function solve_Q3(nNodes, epsilon=0.0001)
     cdn3 = (1/(2* dx*dx*dx)) .* [ -1 2 0 -2 1 ]
     cdn2 = (1/(dx*dx)) .* [ 1 -2 1 ]
     cdn = (1/(2*dx)) .* [ -1 0 1 ]
+
+    println("3rd Derivative Stencil: $cdn3")
+    println("2nd Derivative Stencil: $cdn2")
+    println("1st Derivative Stencil: $cdn")
     
     # Build the linear parts of the matrix (first term of both equations)
     # Make a single stencil, replicate it for each row, adjust the first and last for the boundary conditions
@@ -91,6 +95,9 @@ function solve_Q3(nNodes, epsilon=0.0001)
         end
     end
 
+    # Set the augmented column all to zero
+    matrix[(nVars*nVars)+1:nVars*(nVars+1)] = 0.0
+
     # Add linear boundary conditions!, matbe set the other augmented terms to zero
     println("Linear terms matrix:")
     printMatrix(matrix)
@@ -114,8 +121,6 @@ function solve_Q3(nNodes, epsilon=0.0001)
             return (4*x[nVars-1] - x[nVars-3]) / 3
         elseif i == -1
             return (-4 * x[1] + x[3]) / 3
-
-
         else
             return x[i]
         end
@@ -128,13 +133,19 @@ function solve_Q3(nNodes, epsilon=0.0001)
 
         # Calculate values of nonlinear terms for this iteration, lagging the lowest order terms in each nonlinear term
         for r in 1:nNodes
+            # Add all the 3-element derivative discretizations
             # Adding second derivative of zeta
             cdn2_Z2 = cdn2 .* 3 * getXVal(2*r - 1)
+            println("2nd Derivative of z term: $cdn2_Z2")
             # Adding squared first derivative of zeta
             cdn1_Z1 = cdn .* 2 * (getXVal(2*r + 1) - getXVal(2*r - 3)) / (2*dx)
-            semiSpan = floor(Int32, size(cdn2, 2) -1 /2)
+            println("1st Derivative of z term: $cdn1_Z1")
+            #Adding first derivative of T1
+            cdn_T1 = cdn .* 3 * 0.71 * getXVal(2*r)
+            println("1st Derivative of T term: $cdn_T1")
+            semiSpan = floor(Int32, (size(cdn2, 2) -1) /2)
             for a in 1:size(cdn2, 2)
-                offCenter = a - (semiSpan + 1)
+                offCenter = a - semiSpan
                 col = r + offCenter
                 if col < 1
                     continue
@@ -144,20 +155,12 @@ function solve_Q3(nNodes, epsilon=0.0001)
                 println("Row: $r, Column: $col")
                 matrix[2*r-1, col] += cdn2_Z2[a]
                 matrix[2*r-1, col] += cdn1_Z1[a]
+                matrix[2*r, col] += cdn_T1[a]
             end
 
-            printMatrix(matrixI)
-
-            #Adding first derivative of T1
-            cdn_T1 = cdn .* 3 * 0.71 * x[2*i]
-            offset = size(cdn, 2) - size(cdn3, 2)
-            for a in 1+offset:size(cdn, 2)+offset
-                matrix[2*i, 2*i] += cdn_T1[a]
-            end
-
+            printMatrix(matrix)
         end
 
-        printMatrix(matrix)
         # Adjust first and last rows with boundary conditions
 
 
