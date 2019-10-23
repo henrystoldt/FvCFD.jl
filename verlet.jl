72d0befb5ac7d9b42f3c5789418cf7d54e5f5c7f
hhstoldt <hhstoldt@ucalgary.ca>
2019-10-23 10:19:53 -0600

    Verlet completed, with check

--- verlet.jl
+++ verlet.jl
@@ -1,4 +1,3 @@
-include("ODESolvers.jl")
 using Plots
 using Plots.PlotMeasures
 plotly()
@@ -43,16 +42,16 @@ function solve_Verlet(initXs, initVs, accelFns, timeStep, endTime)
 
     nPos = size(initXs, 1)
     nResultsVars = nPos*3 + 1
-    results = Array{Float64, 2}(undef, nResultsVars, nTimeSteps+1)
+    results = Array{Float64, 2}(undef, nResultsVars, nTimeSteps+2)
     
     iterationCounter = 1
     results[1,1] = 0
 
     # Initialize results matrix with initial values
-    for i in 1:nPos
-        results[i+1, 1] = initXs[i]
-        results[i+1+nPos, 1] = initVs[i]
-        results[i+1+2*nPos, 1] = accelFns[i](initXs, initVs)
+    for var in 1:nPos
+        results[var+1, 1] = initXs[var]
+        results[var+1+nPos, 1] = initVs[var]
+        results[var+1+2*nPos, 1] = accelFns[var](initXs, initVs)
     end
 
     ############ Utility Functions #############
@@ -101,26 +100,26 @@ function solve_Verlet(initXs, initVs, accelFns, timeStep, endTime)
     while currTime + 0.00000001 < endTime
         # Solve for new accelerations and positions, velocity predictor
         predVels = []
-        for i in 1:nPos
-            ax = accel(i, iterationCounter)
-            newX = pos(i, iterationCounter) + vel(i, iterationCounter)*timeStep + 0.5*ax*timeStep*timeStep
-            push!(predVels, vel(i, iterationCounter) + ax*timeStep)
+        for var in 1:nPos
+            ax = accel(var, iterationCounter)
+            newX = pos(var, iterationCounter) + vel(var, iterationCounter)*timeStep + 0.5*ax*timeStep*timeStep
+            push!(predVels, vel(var, iterationCounter) + ax*timeStep)
     
-            storeAccel(i, iterationCounter, ax)
-            storePos(i, iterationCounter+1, newX)
+            storeAccel(var, iterationCounter, ax)
+            storePos(var, iterationCounter+1, newX)
         end
         
-        # Velocity corrector loop
-        for i in 1:nPos
-            ax = results[2*nPos + 1 + i, iterationCounter]
-            ax2 = accelFns[i](getPosVector(iterationCounter+1), predVels)
-            corrVx = vel(i, iterationCounter) + (ax + ax2)*timeStep/2
+        # Velocity corrector
+        for var in 1:nPos
+            ax = results[2*nPos + 1 + var, iterationCounter]
+            ax2 = accelFns[var](getPosVector(iterationCounter+1), predVels)
+            corrVx = vel(var, iterationCounter) + (ax + ax2)*timeStep/2
             
-            storeVel(i, iterationCounter+1, corrVx)
+            storeVel(var, iterationCounter+1, corrVx)
         end
 
         currTime += timeStep
-        results[1, iterationCounter] = currTime
+        results[1, iterationCounter+1] = currTime
         iterationCounter += 1      
     end
 
@@ -128,22 +127,55 @@ function solve_Verlet(initXs, initVs, accelFns, timeStep, endTime)
 end
 
 ########### Call integration function, plot results ##############
+# Set up and solve problem
 initPos = [0, 10]
 initVels = [5, 10]
 accelFns = [dx2dt, dy2dt]
-dt = 0.1
+dt = 0.01
 endTime = 10
 results = solve_Verlet(initPos, initVels, accelFns, dt, endTime)
-nTimeSteps = round(Int32, endTime/dt)
-time = results[1,1:nTimeSteps]
-x = results[2, 1:nTimeSteps]
-y = results[3, 1:nTimeSteps]
-Vx = results[4, 1:nTimeSteps]
-Vy = results[5, 1:nTimeSteps]
 
+# Split results
+nDataPts = ceil(Int32, endTime/dt)+1
+time = results[1,1:nDataPts]
+x = results[2, 1:nDataPts]
+y = results[3, 1:nDataPts]
+Vx = results[4, 1:nDataPts]
+Vy = results[5, 1:nDataPts]
+
+print(time)
+
+# Plot results
 xPlot = plot(time, x, label="X Position", title="Position vs Time", xlabel="t")
 xPlot = plot!(time, y, label="Y Position")
 vPlot = plot(time, Vx, label="X-Velocity", title="Velocity vs Time", xlabel="t")
 vPlot = plot!(time, Vy, label="Y-Velocity")
-plot(xPlot, vPlot, aPlot, size=(1720, 880), window_title="Verlet")
+plot(xPlot, vPlot, size=(1720, 880), window_title="Verlet")
+gui()
+
+
+# Solve with explicit Euler to check velocity result
+include("ODESolvers.jl")
+
+function Euler_dx2dt(vx, vy)
+    # -a/m * sqrt(vx^2 + vy^2)*vx - w*b*vy/m
+    return -0.1*sqrt(vx*vx + vy*vy)*vx - 2*vy
+end
+
+function Euler_dy2dt(vx, vy)
+    # -g -a/m*sqrt(vx^2 + vy^2)vy + w*b*vx/m
+    return -9.81 - 0.1*sqrt(vx*vx + vy*vy)*vy + 2*vx
+end
+
+init = [5,10]
+dt = 0.01
+timeSteps = 1000
+ddts = [ Euler_dx2dt, Euler_dy2dt ]
+results = solve_ExplicitEuler(init, ddts, dt, timeSteps)
+
+Vx = results[1, :]
+Vy = results[2, :]
+time = results[3, :]
+vPlot = plot(time, Vx, label="X-Velocity", title="Velocity vs Time", xlabel="t", size=(860, 880), window_title="Verlet R")
+vPlot = plot!(time, Vy, label="Y-Velocity")
 gui()
