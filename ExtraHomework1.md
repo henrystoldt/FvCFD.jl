a93da7094f3b88deec943287a36d0487f8455c2c
hhstoldt <hhstoldt@ucalgary.ca>
2019-10-23 02:01:00 -0600

    Verlet Complete

--- ExtraHomework1.md
+++ ExtraHomework1.md
@@ -4,8 +4,14 @@ ID: 10127324
 Date: Thursday Oct. 24th, 2019  
 Language: Julia 
 
-The file "Assignment 3.jl" runs all of code requested in Q1-3. Explanation and comments below.
+The file "verlet.jl" runs all of code requested for Additional Homework #1. Explanation and comments below.  
+Plots and code are attached.
 
-## Question 1:
-### The Solvers:
-### Solving the sample matrix:
+### The Solver:
+I've constructed a verlet position integrator for n spatial coordinates. It works based on the process outlined in the additional notes Dr. Mohamad posted on D2l (section 7.5.4, "The Verlet Method").
+The algorithm first calculates the position at time i+1 explicitly, using the position, velocity and accelerations from time i.
+Next, it calculates the velocity at time i+1 implicitly, in my case using a predictor-corrector method.
+The predictor corrector method I implemented predicts the velocity at time i+1 using the explicit first-order euler method, then corrects it using equation 7.52 from the provided notes, where the acceleration at time i+1 is calculated using the predicted values of the velocity.
+
+### Solving the sample problem:
+As requested, the sample problem was solved for the time interval 0-10 seconds. Time steps smaller than the suggested value of 0.1 seconds did not noticeably change the solution.
--- verlet.jl
+++ verlet.jl
@@ -10,112 +10,140 @@ plotly()
 # g = 9.81
 # b = 1
 
-function dx2dt(vx, vy)
+function dx2dt(initXs, initVs)
     # -a/m * sqrt(vx^2 + vy^2)*vx - w*b*vy/m
-    return -5 #-0.1*sqrt(vx*vx + vy*vy)*vx - 2*vy
+    vx = initVs[1]
+    vy = initVs[2]
+    return -0.1*sqrt(vx*vx + vy*vy)*vx - 2*vy
 end
 
-function dy2dt(vx, vy)
+function dy2dt(initXs, initVs)
     # -g -a/m*sqrt(vx^2 + vy^2)vy + w*b*vx/m
-    return -10 #-9.81 - 0.1*sqrt(vx*vx + vy*vy)*vy + 2*vx
+    vx = initVs[1]
+    vy = initVs[2]
+    return -9.81 - 0.1*sqrt(vx*vx + vy*vy)*vy + 2*vx
 end
 
-function solve_Verlet(x1, y1, vx1, vy1, timeStep, endTime)
+########### Verlet Motion Solver #############
+# Pass in arrays of initial positions, velocities, and acceleration functions
+    # Acceleration functions must accept the positions and velocities as arguments
+# Returns results matrix, with rows containing the folowing, with N coordinate directions
+    # Time values
+    # X1 Position
+    # ...
+    # XN Position
+    # X1 Velocity
+    # ...
+    # XN Velocity
+    # X1 Accel
+    # ...
+    # XN Accel
+function solve_Verlet(initXs, initVs, accelFns, timeStep, endTime)
     nTimeSteps = ceil(Int32, endTime/timeStep)
-    results = Array{Float64, 2}(undef, 7, nTimeSteps+1)
+
+    nPos = size(initXs, 1)
+    nResultsVars = nPos*3 + 1
+    results = Array{Float64, 2}(undef, nResultsVars, nTimeSteps+1)
     
     iterationCounter = 1
-    results[1,iterationCounter] = 0
-    results[2,iterationCounter] = x1
-    results[3,iterationCounter] = y1
-    results[4,iterationCounter] = vx1
-    results[5,iterationCounter] = vy1
-    results[6,iterationCounter] = dx2dt(vx1, vy1)
-    results[7,iterationCounter] = dy2dt(vx1, vy1)
+    results[1,1] = 0
 
-    # Compute second set of values to start the method
-    iterationCounter += 1
-    currTime = timeStep
-    x2 = x1 + vx1*timeStep + 0.5*results[6,1]*timeStep*timeStep
-    y2 = y1 + vy1*timeStep + 0.5*results[7,1]*timeStep*timeStep
-    predVx = vx1 + results[6,1]*timeStep
-    predVy = vy1 + results[7,1]*timeStep
-    ax2 = dx2dt(predVx, predVy)
-    ay2 = dy2dt(predVx, predVy)
-    corrVx = vx1 + (results[6,1] + ax2)*0.5*timeStep
-    corrVy = vy1 + (results[7,1] + ay2)*0.5*timeStep
+    # Initialize results matrix with initial values
+    for i in 1:nPos
+        results[i+1, 1] = initXs[i]
+        results[i+1+nPos, 1] = initVs[i]
+        results[i+1+2*nPos, 1] = accelFns[i](initXs, initVs)
+    end
 
-    results[1,iterationCounter] = timeStep
-    results[2,iterationCounter] = x2
-    results[3,iterationCounter] = y2
-    results[4,iterationCounter] = corrVx
-    results[5,iterationCounter] = corrVy
-    results[6,iterationCounter] = ax2
-    results[7,iterationCounter] = ay2
+    ############ Utility Functions #############
+    function vel(var, iteration) 
+        return results[var+1+nPos, iteration]
+    end
 
+    function pos(var, iteration)
+        return results[var+1, iteration]
+    end
+
+    function accel(var, iteration)
+        return accelFns[var](getPosVector(iteration), getVelVector(iteration))
+    end
+
+    function getPosVector(iteration)
+        pstns = []
+        for i in 1:nPos
+            push!(pstns, results[i+1, iteration])
+        end
+        return pstns
+    end
+
+    function getVelVector(iteration)
+        vels = []
+        for i in 1:nPos
+            push!(vels, results[i+nPos+1, iteration])
+        end
+        return vels
+    end
+
+    function storePos(var, iteration, pos)
+        results[var+1, iteration] = pos
+    end
+
+    function storeVel(var, iteration, vel)
+        results[var+nPos+1, iteration] = vel
+    end
+
+    function storeAccel(var, iteration, accel)
+        results[var + 2*nPos + 1, iteration] = accel
+    end
+
+    ########## Verlet integration loop #############
+    currTime = 0
     while currTime + 0.00000001 < endTime
-        # Solve for new accelerations and positions
-        ax = dx2dt(results[4, iterationCounter], results[5, iterationCounter])
-        newX = results[2, iterationCounter] + results[4, iterationCounter]*timeStep + 0.5*ax*timeStep*timeStep
-
-        ay = dy2dt(results[4, iterationCounter], results[5, iterationCounter])
-        newY = results[3, iterationCounter] + results[5, iterationCounter]*timeStep + 0.5*ay*timeStep*timeStep
-
-        # Solve for v @ i+1 using predictor-corrector method
-        # Predictor
-        lastVx = results[4, iterationCounter]
-        predNewVx = lastVx + ax*timeStep
-        lastVy = results[5, iterationCounter]
-        predNewVy = lastVy + ay*timeStep
+        # Solve for new accelerations and positions, velocity predictor
+        predVels = []
+        for i in 1:nPos
+            ax = accel(i, iterationCounter)
+            newX = pos(i, iterationCounter) + vel(i, iterationCounter)*timeStep + 0.5*ax*timeStep*timeStep
+            push!(predVels, vel(i, iterationCounter) + ax*timeStep)
+    
+            storeAccel(i, iterationCounter, ax)
+            storePos(i, iterationCounter+1, newX)
+        end
         
-        # Corrector
-        newAx = dx2dt(predNewVx, predNewVy)
-        newAy = dy2dt(predNewVx, predNewVy)
-        newVx = lastVx + (ax + newAx)*timeStep/2
-        newVy = lastVy + (ay + newAy)*timeStep/2
+        # Velocity corrector loop
+        for i in 1:nPos
+            ax = results[2*nPos + 1 + i, iterationCounter]
+            ax2 = accelFns[i](getPosVector(iterationCounter+1), predVels)
+            corrVx = vel(i, iterationCounter) + (ax + ax2)*timeStep/2
+            
+            storeVel(i, iterationCounter+1, corrVx)
+        end
 
         currTime += timeStep
-        iterationCounter += 1
-        
         results[1, iterationCounter] = currTime
-        results[2, iterationCounter] = newX
-        results[3, iterationCounter] = newY
-        results[4, iterationCounter] = newVx
-        results[5, iterationCounter] = newVy
-        results[6, iterationCounter] = ax
-        results[7, iterationCounter] = ay        
+        iterationCounter += 1      
     end
 
     return results
 end
 
-results = verlet(0, 10, 5, 10, 0.01, 10)
-time = results[1,:]
-x = results[2, :]
-y = results[3, :]
-Vx = results[4, :]
-Vy = results[5, :]
-ax = results[6, :]
-ay = results[7, :]
+########### Call integration function, plot results ##############
+initPos = [0, 10]
+initVels = [5, 10]
+accelFns = [dx2dt, dy2dt]
+dt = 0.1
+endTime = 10
+results = solve_Verlet(initPos, initVels, accelFns, dt, endTime)
+nTimeSteps = round(Int32, endTime/dt)
+time = results[1,1:nTimeSteps]
+x = results[2, 1:nTimeSteps]
+y = results[3, 1:nTimeSteps]
+Vx = results[4, 1:nTimeSteps]
+Vy = results[5, 1:nTimeSteps]
 
 xPlot = plot(time, x, label="X Position", title="Position vs Time", xlabel="t")
 xPlot = plot!(time, y, label="Y Position")
 vPlot = plot(time, Vx, label="X-Velocity", title="Velocity vs Time", xlabel="t")
 vPlot = plot!(time, Vy, label="Y-Velocity")
-aPlot = plot(time, ax, label="X-Accel", title="Acceleration vs Time", xlabel="t")
-aPlot = plot!(time, ay, label="Y-Accel")
-plot(xPlot, vPlot, aPlot, size=(1720, 880), window_title="Verlet R")
+plot(xPlot, vPlot, aPlot, size=(1720, 880), window_title="Verlet")
 gui()
-
-# Solve with explicit Euler to check
-# init = [5,10]
-# dt = 0.01
-# timeSteps = 1000
-# ddts = [ dx2dt, dy2dt ]
-# results = solve_ExplicitEuler(init, ddts, dt, timeSteps)
-# Vx = results[1, :]
-# Vy = results[2, :]
-# time = results[3,:]
-# vPlot = plot(time, Vx, label="X-Velocity", title="Velocity vs Time", xlabel="t", size=(860, 880), window_title="Verlet R")
-# vPlot = plot!(time, Vy, label="Y-Velocity")
-# gui()
