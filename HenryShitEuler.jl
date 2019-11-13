72f705e2915d305484d19275b2ba84f5b8b0bdcb
henrystoldt <henrystoldt@users.noreply.github.com>
2019-11-13 04:33:43 -0800

    Split MacCormack Code into functions

--- HenryShitEuler.jl
+++ HenryShitEuler.jl
@@ -26,7 +26,37 @@ function calPerfectT(e, Cp=1005)
 end
 
 ######################### Gradient Computation #######################
-#TODO
+function forwardGradient(dx, values...)
+    result = []
+    for vals in values
+        n = size(vals,1)
+        grad = Array{Float64, 1}(undef, n)
+        grad[n] = 0
+        for i in 1:n-1
+            grad[i] = (vals[i+1] - vals[i]) / dx[i]
+        end
+        push!(result, grad)
+    end
+    return result
+end
+
+function backwardGradient(dx, values...)
+    result = []
+    for vals in values
+        n = size(vals,1)
+        grad = Array{Float64, 1}(undef, n)
+        grad[1] = 0
+        for i in 2:n
+            grad[i] = (vals[i] - vals[i-1])/dx[i-1]
+        end
+        push!(result, grad)
+    end
+    return result
+end
+
+#TODO: Upwind gradients
+#TODO: TVD gradients
+#TODO: Central gradient
 
 ######################### Boundary Conditions #######################
 function copyValues(fromIndex, toIndex, varArrays)
@@ -36,18 +66,13 @@ function copyValues(fromIndex, toIndex, varArrays)
 end
 
 ######################### Initialization #######################
-function doCFD()
+function initializeShockTube(nCells=100, domainLength=1)
     # Create arrays to store data (cell # = position in array)
-    nCells = 100
-    domainLength = 1 #m
-    dt = 0.0001
-    # endTime = 0.1
-    endTime = 0.14267 #sec
-    cellSize = Array{Float64, 1}(undef, nCells)
+    dx = Array{Float64, 1}(undef, nCells)
     rho = Array{Float64, 1}(undef, nCells)
+    U = Array{Float64, 1}(undef, nCells)
     e = Array{Float64, 1}(undef, nCells)
     P = Array{Float64, 1}(undef, nCells)
-    U = Array{Float64, 1}(undef, nCells)
     T = Array{Float64, 1}(undef, nCells)
 
 
@@ -55,7 +80,7 @@ function doCFD()
     for i in 1:nCells
         LHSRho = idealGasRho(0.00348432, 1)
         LHSe = calPerfectEnergy(0.00348432)
-        RHSRho = idealGasRho(0.00278746, 0.5)
+        RHSRho = idealGasRho(0.00278746, 0.1)
         RHSe = calPerfectEnergy(0.00278746)
 
         if i <= (nCells/2)
@@ -70,11 +95,19 @@ function doCFD()
         T[i] = calPerfectT(e[i])
         P[i] = idealGasP(rho[i], T[i])
 
-        cellSize[i] = domainLength / nCells
+        # dx = Distance between cell centers i and i+1
+        dx[i] = domainLength / nCells
     end
 
+    return dx, rho, U, e, P, T
+end
 
-    ######################### Solution #######################
+######################### Solution #######################
+# Pass in initial values for each variable
+# Shock Tube (undisturbed zero gradient) boundary conditions assumed
+function macCormack1D(dx, rho, U, e, P, T, dt=0.0001, endTime=0.14267)
+    nCells = size(rho, 1)
+    
     drhoPred = Array{Float64, 1}(undef, nCells)
     duPred = Array{Float64, 1}(undef, nCells)
     dePred = Array{Float64, 1}(undef, nCells)
@@ -91,20 +124,17 @@ function doCFD()
             dt = endTime - currTime
         end
 
+        ############## Predictor #############
+        # Forward Differences to compute gradients
+        drhodx, dudx, dpdx, dedx = forwardGradient(dx, rho, U, P, e)
         for i in 2:(nCells-1)
-            # Forward Differences to compute gradients
-            dx = (cellSize[i] + cellSize[i+1]) / 2
-            drhodx = (rho[i+1] - rho[i]) / dx
-            dudx = (U[i+1] - U[i]) / dx
-            dpdx = (P[i+1] - P[i]) / dx
-            dedx = (e[i+1] - e[i]) / dx
 
             # Eq. 6.1
-            drhoPred[i] = -(rho[i]*dudx + U[i]*drhodx)
+            drhoPred[i] = -(rho[i]*dudx[i] + U[i]*drhodx[i])
             # Eq. 6.2
-            duPred[i] = -(U[i]*dudx + dpdx/rho[i])
+            duPred[i] = -(U[i]*dudx[i] + dpdx[i]/rho[i])
             # Eq. 6.4
-            dePred[i] = -(U[i]*dedx + P[i]*dudx/rho[i])
+            dePred[i] = -(U[i]*dedx[i] + P[i]*dudx[i]/rho[i])
 
             rhoPred[i] = rho[i] + drhoPred[i]*dt
             uPred[i] = U[i] + duPred[i]*dt
@@ -113,21 +143,19 @@ function doCFD()
             PPred[i] = idealGasP(rhoPred[i], TPred[i])
         end
 
+        ############### Corrector ################
+        # Rearward differences to compute gradients
+        drhodx, dudx, dpdx, dedx = backwardGradient(dx, rhoPred, uPred, PPred, ePred)
         for i in 2:(nCells-1)
-            # Rearward differences to compute gradients
-            dx = (cellSize[i] + cellSize[i-1]) / 2
-            dudx = (uPred[i] - uPred[i-1]) / dx
-            dpdx = (PPred[i] - PPred[i-1]) / dx
-            drhodx = (rhoPred[i] - rhoPred[i-1]) / dx
-            dedx = (ePred[i] - ePred[i-1]) / dx
 
             # Eq. 6.1
-            drhoPred2 = -(rhoPred[i]*dudx + uPred[i]*drhodx)
+            drhoPred2 = -(rhoPred[i]*dudx[i] + uPred[i]*drhodx[i])
             # Eq. 6.2
-            duPred2 = -(uPred[i]*dudx + dpdx/rhoPred[i])
+            duPred2 = -(uPred[i]*dudx[i] + dpdx[i]/rhoPred[i])
             # Eq. 6.4
-            dePred2 = -(uPred[i]*dedx + PPred[i]*dudx/rhoPred[i])
+            dePred2 = -(uPred[i]*dedx[i] + PPred[i]*dudx[i]/rhoPred[i])
 
+            # Perform timestep using average gradients
             rho[i] += (drhoPred2 + drhoPred[i])*dt/2
             U[i] += (duPred2 + duPred[i])*dt/2
             e[i] += (dePred2 + dePred[i])*dt/2
@@ -135,6 +163,7 @@ function doCFD()
             P[i] = idealGasP(rho[i], T[i])
         end
 
+        ############### Boundaries ################
         # Waves never reach the boundaries, so boundary treatemnt doesn't need to be good
         allVars = [ rho, U, e, T, P ]
         copyValues(3, 2, allVars)
@@ -148,8 +177,9 @@ function doCFD()
     return P, U, T, rho
 end
 
-P, U, T, rho= doCFD()
+P, U, T, rho = macCormack1D(initializeShockTube()...)
 
+################## Output ##################
 plots = []
 pPlot = plot(P, label="P (Pa)", title="Pressure", xlabel="x (m)", bottom_margin=15mm, left_margin=10mm)
 rhoPlot = plot(rho, label="rho (kg/m3)", title="Density", xlabel="x (m)", bottom_margin=15mm, left_margin=10mm)
