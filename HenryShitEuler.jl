f1d3dd3c6fdcbce0514ba38edf3ddeaf6a00ef93
henrystoldt <henrystoldt@users.noreply.github.com>
2019-11-14 15:43:56 -0800

    Split out gradient fns, in progress conservative form

--- HenryShitEuler.jl
+++ HenryShitEuler.jl
@@ -54,9 +54,49 @@ function backwardGradient(dx, values...)
     return result
 end
 
-#TODO: Upwind gradients
+function centralGradient(dx, values...)
+    result = []
+    for vals in values
+        n = size(vals,1)
+        grad = Array{Float64, 1}(undef, n)
+        grad[1] = 0
+        grad[n] = 0
+        for i in 2:n-1
+            grad[i] = (vals[i+1] - vals[i-1])/(dx[i] + dx[i-1])
+        end
+        push!(result, grad)
+    end
+    return result
+end
+
+function upwindGradient(dx, U, values...)
+    result = []
+    for vals in values
+        n = size(vals,1)
+        grad = Array{Float64, 1}(undef, n)
+
+        if U[1] > 0
+            grad[1] = 0
+        end
+        if U[n] <= 0
+            grad[n] = 0
+        end
+        
+        for i in 1:n
+            if U[i] > 0 && i > 1
+                grad[i] = (vals[i] - vals[i-1])/dx[i-1]
+            elseif U[i] == 0 && i > 1 && i < n
+                grad[i] = (vals[i+1] - vals[i-1]) / (dx[i-1] + dx[i])
+            elseif i < n
+                grad[i] = (vals[i+1] - vals[i])/dx[i]
+            end
+        end
+        push!(result, grad)
+    end
+    return result
+end
+
 #TODO: TVD gradients
-#TODO: Central gradient
 
 ######################### Boundary Conditions #######################
 function copyValues(fromIndex, toIndex, varArrays)
@@ -105,7 +145,7 @@ end
 ######################### Solution #######################
 # Pass in initial values for each variable
 # Shock Tube (undisturbed zero gradient) boundary conditions assumed
-function macCormack1D(dx, rho, U, e, P, T, dt=0.0001, endTime=0.14267)
+function macCormack1D(dx, rho, U, e, P, T; dt=0.001, endTime=0.14267)
     nCells = size(rho, 1)
     
     drhoPred = Array{Float64, 1}(undef, nCells)
@@ -114,7 +154,7 @@ function macCormack1D(dx, rho, U, e, P, T, dt=0.0001, endTime=0.14267)
 
     rhoPred = Array{Float64, 1}(undef, nCells)
     TPred = Array{Float64, 1}(undef, nCells)
-    uPred = Array{Float64, 1}(undef, nCells)
+    UPred = Array{Float64, 1}(undef, nCells)
     PPred = Array{Float64, 1}(undef, nCells)
     ePred = Array{Float64, 1}(undef, nCells)
 
@@ -126,7 +166,9 @@ function macCormack1D(dx, rho, U, e, P, T, dt=0.0001, endTime=0.14267)
 
         ############## Predictor #############
         # Forward Differences to compute gradients
-        drhodx, dudx, dpdx, dedx = forwardGradient(dx, rho, U, P, e)
+        drhodx, dudx, dpdx, dedx = backwardGradient(dx, rho, U, P, e)
+        # drhodx, dudx, dpdx, dedx = upwindGradient(dx, U, rho, U, P, e)
+        # drhodx, dudx, dpdx, dedx = centralGradient(dx, rho, U, P, e)
         for i in 2:(nCells-1)
 
             # Eq. 6.1
@@ -137,7 +179,7 @@ function macCormack1D(dx, rho, U, e, P, T, dt=0.0001, endTime=0.14267)
             dePred[i] = -(U[i]*dedx[i] + P[i]*dudx[i]/rho[i])
 
             rhoPred[i] = rho[i] + drhoPred[i]*dt
-            uPred[i] = U[i] + duPred[i]*dt
+            UPred[i] = U[i] + duPred[i]*dt
             ePred[i] = e[i] + dePred[i]*dt
             TPred[i] = calPerfectT(ePred[i])
             PPred[i] = idealGasP(rhoPred[i], TPred[i])
@@ -145,15 +187,17 @@ function macCormack1D(dx, rho, U, e, P, T, dt=0.0001, endTime=0.14267)
 
         ############### Corrector ################
         # Rearward differences to compute gradients
-        drhodx, dudx, dpdx, dedx = backwardGradient(dx, rhoPred, uPred, PPred, ePred)
+        drhodx, dudx, dpdx, dedx = forwardGradient(dx, rhoPred, UPred, PPred, ePred)
+        # drhodx, dudx, dpdx, dedx = upwindGradient(dx, UPred, rhoPred, UPred, PPred, ePred)
+        # drhodx, dudx, dpdx, dedx = centralGradient(dx, rhoPred, UPred, PPred, ePred)
         for i in 2:(nCells-1)
 
             # Eq. 6.1
-            drhoPred2 = -(rhoPred[i]*dudx[i] + uPred[i]*drhodx[i])
+            drhoPred2 = -(rhoPred[i]*dudx[i] + UPred[i]*drhodx[i])
             # Eq. 6.2
-            duPred2 = -(uPred[i]*dudx[i] + dpdx[i]/rhoPred[i])
+            duPred2 = -(UPred[i]*dudx[i] + dpdx[i]/rhoPred[i])
             # Eq. 6.4
-            dePred2 = -(uPred[i]*dedx[i] + PPred[i]*dudx[i]/rhoPred[i])
+            dePred2 = -(UPred[i]*dedx[i] + PPred[i]*dudx[i]/rhoPred[i])
 
             # Perform timestep using average gradients
             rho[i] += (drhoPred2 + drhoPred[i])*dt/2
@@ -177,7 +221,140 @@ function macCormack1D(dx, rho, U, e, P, T, dt=0.0001, endTime=0.14267)
     return P, U, T, rho
 end
 
-P, U, T, rho = macCormack1D(initializeShockTube()...)
+#TODO: Min initial conditions
+function macCormack1DConservative(dx, rho, U, e, P, T; dt=0.001, endTime=0.14267)
+    nCells = size(rho, 1)
+
+    xMom = Array{Float64, 1}(undef, nCells)
+    eV2 = Array{Float64, 1}(undef, nCells)
+
+    #TODO: calc xMom and eV2 from initCond
+    
+    drhoPred = Array{Float64, 1}(undef, nCells)
+    dxMPred = Array{Float64, 1}(undef, nCells)
+    deVPred = Array{Float64, 1}(undef, nCells)
+
+    rhoPred = Array{Float64, 1}(undef, nCells)
+    UPred = Array{Float64, 1}(undef, nCells)
+    PPred = Array{Float64, 1}(undef, nCells)
+    ePred = Array{Float64, 1}(undef, nCells)
+
+    currTime = 0
+    while currTime < endTime
+        if (endTime - currTime) < dt
+            dt = endTime - currTime
+        end
+
+        ############## Predictor #############
+        # Forward Differences to compute gradients
+        drhodx, dudx, dpdx, dedx, deVdx = backwardGradient(dx, rho, U, P, e, eV2)
+        # drhodx, dudx, dpdx, dedx = upwindGradient(dx, U, rho, U, P, e)
+        # drhodx, dudx, dpdx, dedx = centralGradient(dx, rho, U, P, e)
+
+        for i in 2:(nCells-1)
+
+            # Eq. 2.82b
+            drhoPred[i] = -(rho[i]*dudx[i] + U[i]*drhodx[i])
+            # Eq. 2.84a
+            dxMPred[i] = -(dpdx[i] +  2*rho[i]*U[i]*dudx[i] + U[i]*U[i]*drhodx[i])
+            # Eq. 2.86
+            deVPred[i] = -(U[i]*dpdx[i] + P[i]*dudx[i] + eV2[i]*drhodx[i]*U[i] + rho[i]*deVdx[i]*U[i] + rho[i]*eV2[i]*dudx[i])
+
+            rhoPred[i] = rho[i] + drhoPred[i]*dt
+            UPred[i] = U[i] + duPred[i]*dt
+            ePred[i] = e[i] + dePred[i]*dt
+            TPred[i] = calPerfectT(ePred[i])
+            PPred[i] = idealGasP(rhoPred[i], TPred[i])
+        end
+
+        ############### Corrector ################
+        # Rearward differences to compute gradients
+        drhodx, dudx, dpdx, dedx = forwardGradient(dx, rhoPred, UPred, PPred, ePred)
+        # drhodx, dudx, dpdx, dedx = upwindGradient(dx, UPred, rhoPred, UPred, PPred, ePred)
+        # drhodx, dudx, dpdx, dedx = centralGradient(dx, rhoPred, UPred, PPred, ePred)
+
+        #TODO: Update block
+        for i in 2:(nCells-1)
+
+            # Eq. 6.1
+            drhoPred2 = -(rhoPred[i]*dudx[i] + UPred[i]*drhodx[i])
+            # Eq. 6.2
+            duPred2 = -(UPred[i]*dudx[i] + dpdx[i]/rhoPred[i])
+            # Eq. 6.4
+            dePred2 = -(UPred[i]*dedx[i] + PPred[i]*dudx[i]/rhoPred[i])
+
+            # Perform timestep using average gradients
+            rho[i] += (drhoPred2 + drhoPred[i])*dt/2
+            U[i] += (duPred2 + duPred[i])*dt/2
+            e[i] += (dePred2 + dePred[i])*dt/2
+            T[i] = calPerfectT(e[i])
+            P[i] = idealGasP(rho[i], T[i])
+        end
+
+        ############### Boundaries ################
+        # Waves never reach the boundaries, so boundary treatemnt doesn't need to be good
+        #TODO: Update
+        allVars = [ rho, U, e, T, P ]
+        copyValues(3, 2, allVars)
+        copyValues(2, 1, allVars)
+        copyValues(nCells-2, nCells-1, allVars)
+        copyValues(nCells, nCells, allVars)
+        
+        currTime += dt
+    end
+
+    return P, U, T, rho
+end
+
+# Pass in initial values for each variable
+# Shock Tube (undisturbed zero gradient) boundary conditions assumed
+function upwind1D(dx, rho, U, e, P, T; dt=0.001, endTime=0.14267)
+    nCells = size(rho, 1)
+    
+    drhoPred = Array{Float64, 1}(undef, nCells)
+    duPred = Array{Float64, 1}(undef, nCells)
+    dePred = Array{Float64, 1}(undef, nCells)
+
+    currTime = 0
+    while currTime < endTime
+        if (endTime - currTime) < dt
+            dt = endTime - currTime
+        end
+
+        ############## Predictor #############
+        # Forward Differences to compute gradients
+        # drhodx, dudx, dpdx, dedx = backwardGradient(dx, rho, U, P, e)
+        drhodx, dudx, dpdx, dedx = upwindGradient(dx, U, rho, U, P, e)
+        # drhodx, dudx, dpdx, dedx = centralGradient(dx, rho, U, P, e)
+        for i in 2:(nCells-1)
+            drhoPred[i] = -(rho[i]*dudx[i] + U[i]*drhodx[i])
+            duPred[i] = -(U[i]*dudx[i] + dpdx[i]/rho[i])
+            dePred[i] = -(U[i]*dedx[i] + P[i]*dudx[i]/rho[i])
+
+            rho[i] = rho[i] + drhoPred[i]*dt
+            U[i] = U[i] + duPred[i]*dt
+            e[i] = e[i] + dePred[i]*dt
+            T[i] = calPerfectT(e[i])
+            P[i] = idealGasP(rho[i], T[i])
+        end
+
+        ############### Boundaries ################
+        # Waves never reach the boundaries, so boundary treatemnt doesn't need to be good
+        allVars = [ rho, U, e, T, P ]
+        copyValues(3, 2, allVars)
+        copyValues(2, 1, allVars)
+        copyValues(nCells-2, nCells-1, allVars)
+        copyValues(nCells, nCells, allVars)
+        
+        currTime += dt
+    end
+
+    return P, U, T, rho
+end
+
+P, U, T, rho = upwind1D(initializeShockTube()..., endTime=0.01)
+
+print(P)
 
 ################## Output ##################
 plots = []
