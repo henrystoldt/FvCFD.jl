7570bc6d16c2b2a7c67d8e74c7a2d9d0f8e826ff
henrystoldt <henrystoldt@users.noreply.github.com>
2019-11-25 17:13:01 +1100

    Created separate runscript

--- HEuler.jl
+++ HEuler.jl
@@ -871,18 +871,3 @@ function upwindFVM(mesh, P, T, U; initDt=0.001, endTime=0.14267, targetCFL=0.2, 
 end
 
 #TODO: Proper boundary treatment
-
-################## Output ##################
-nCells = 500
-# P, U, T, rho = macCormack1DFDM(initializeShockTubeFDM(nCells)..., initDt=0.00000001, endTime=0.14267)
-# P, U, T, rho = macCormack1DConservativeFDM(initializeShockTubeFDM(nCells)..., initDt=0.00001, endTime=0.1, Cx=0.3)
-# P, U, T, rho = upwind1DConservativeFDM(initializeShockTubeFDM(nCells)..., initDt=0.00001, endTime=0.1, targetCFL=0.01, Cx=0.3)
-# xVel = U
-
-# P, U, T, rho = upwindFVM(initializeShockTubeFVM(nCells)..., initDt=0.0000001, endTime=0.14267, targetCFL=0.1, Cx=0.3)
-# xVel = Array{Float64, 1}(undef, nCells)
-# for i in 1:nCells
-#     xVel[i] = U[i][1]
-# end
-
-# plotShockTubeResults(P, xVel, T, rho)
