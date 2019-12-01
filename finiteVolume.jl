f4b138dd76cd96f6d0d528f99c01d87d2625c1b3
henrystoldt <henrystoldt@users.noreply.github.com>
2019-12-01 08:13:29 -0700

    Removed 1D JST and timestepping from fvm.jl

--- finiteVolume.jl
+++ finiteVolume.jl
@@ -1,6 +1,8 @@
 using Printf
 include("constitutiveRelations.jl")
 include("vectorFunctions.jl")
+include("timeDiscretizations.jl")
+include("JST_structured_1D.jl")
 
 __precompile__()
 
@@ -290,159 +292,8 @@ function linInterp(mesh, values...)
     return result
 end
 
-function structured_1DlinInterp(dx, values...)
-    result = []
-    nFaces = size(dx, 1) + 1
-
-    for vals in values
-        fVals = []
-
-        ############ Default Value ############
-        # Construct a default value of the appropriate dimensionality
-        # defaultVal = 0.0 for 1D, [ 0.0, 0.0 ] for 2D etc...
-        defaultVal = []
-        if size(vals[1], 1) ==1
-            defaultVal = 0
-        else
-            for a in vals[1]
-                push!(defaultVal, 0.0)
-            end
-        end
-
-        push!(fVals, defaultVal)
-        ########### Do Interpolations ###########
-        for i in 2:nFaces-1
-            c1Dist = dx[i-1]/2
-            c2Dist = dx[i]/2
-            totalDist = c1Dist + c2Dist
-            push!(fVals, vals[i-1].*(c2Dist/totalDist) .+ vals[i].*(c1Dist/totalDist))
-        end
-        push!(fVals, defaultVal)
-
-        push!(result, fVals)
-    end
-    return result
-end
-
-function structured_1DMaxInterp(dx, values...)
-    result = []
-    nFaces = size(dx, 1) + 1
-
-    for vals in values
-        fVals = []
-
-        ############ Default Value ############
-        # Construct a default value of the appropriate dimensionality
-        # defaultVal = 0.0 for 1D, [ 0.0, 0.0 ] for 2D etc...
-        defaultVal = []
-        if size(vals[1], 1) ==1
-            defaultVal = 0
-        else
-            for a in vals[1]
-                push!(defaultVal, 0.0)
-            end
-        end
-
-        push!(fVals, defaultVal)
-        ########### Do Interpolations ###########
-        for i in 2:nFaces-1
-            push!(fVals, max(vals[i-1], vals[i]))
-        end
-        push!(fVals, defaultVal)
-
-        push!(result, fVals)
-    end
-    return result
-end
-
-function structured_1DFaceDelta(dx, values...)
-    result = []
-    nFaces = size(dx, 1) + 1
-
-    for vals in values
-        fVals = []
-
-        ############ Default Value ############
-        # Construct a default value of the appropriate dimensionality
-        # defaultVal = 0.0 for 1D, [ 0.0, 0.0 ] for 2D etc...
-        defaultVal = []
-        if size(vals[1], 1) ==1
-            defaultVal = 0
-        else
-            for a in vals[1]
-                push!(defaultVal, 0.0)
-            end
-        end
-
-        push!(fVals, defaultVal)
-        ########### Do Interpolations ###########
-        for f in 2:nFaces-1
-            push!(fVals, vals[f] - vals[f-1])
-        end
-        push!(fVals, defaultVal)
-
-        push!(result, fVals)
-    end
-    return result
-end
-
 # Matrix versions
 # results are stored in faceValues vectors
-function checkInputs_structured1DInterp(dx, faceValues::Array{Float64,2}, cellValues::Array{Float64, 2})
-    nCells = size(cellValues, 1)
-    nFaces = size(faceValues, 1)
-    nVars = size(faceValues, 2)
-    nVars2 = size(cellValues, 2)
-    if nCells != nFaces-1 || nVars2 != nVars
-        throw(ArgumentError("ERROR: nCells ($nCells) != nFaces ($nFaces) or Number of variables stored in cells($nVars2) and faces($nVars) are not equal!"))
-    end
-    nDxs = size(dx, 1)
-    if nDxs != nCells
-        throw(ArgumentError("ERROR: nCells ($nCells) != nDxs ($nDxs)"))
-    end
-end
-
-function structured_1DlinInterp(dx, faceValues::Array{Float64,2}, cellValues::Array{Float64, 2})
-    nFaces = size(faceValues, 1)
-    nVars = size(faceValues, 2)
-    checkInputs_structured1DInterp(dx, faceValues, cellValues)
-
-    # Do interpolation
-    for f in 2:nFaces-1
-        for v in 1:nVars
-            c1Dist = dx[f-1]/2
-            c2Dist = dx[f]/2
-            totalDist = c1Dist + c2Dist
-            faceValues[f, v] = cellValues[f-1, v].*(c2Dist/totalDist) .+ cellValues[f, v].*(c1Dist/totalDist)
-        end
-    end
-end
-
-function structured_1DMaxInterp(dx, faceValues::Array{Float64,2}, cellValues::Array{Float64, 2})
-    nFaces = size(faceValues, 1)
-    nVars = size(faceValues, 2)
-    checkInputs_structured1DInterp(dx, faceValues, cellValues)
-
-    # Do interpolation
-    for f in 2:nFaces-1
-        for v in 1:nVars
-            faceValues[f, v] = max(cellValues[f-1,v],  cellValues[f,v])
-        end
-    end
-end
-
-function structured_1DFaceDelta(dx, faceValues::Array{Float64,2}, cellValues::Array{Float64, 2})
-    nFaces = size(faceValues, 1)
-    nVars = size(faceValues, 2)
-    checkInputs_structured1DInterp(dx, faceValues, cellValues)
-
-    # Do interpolation
-    for f in 2:nFaces-1
-        for v in 1:nVars
-            faceValues[f, v] = cellValues[f, v] - cellValues[f-1, v]
-        end
-    end
-end
 
 # TODO: TVD Interp
 
@@ -460,77 +311,6 @@ function macCormackAD_S(mesh, C, P, lapl_P)
     end
 
     return S
-end
-
-# Pass in pressure or density for the pressure-based or density-based version respectively
-# Density-based version detects contact discontinuities
-function structured_1D_JST_sj2(dvar)
-    nCells = size(dvar, 1)
-    sj = Array{Float64, 1}(undef, nCells)
-
-    # Boundary values unset
-    sj[1] = 0.0
-    sj[nCells] = 0.0
-
-    for c in 2:nCells-1
-        sj[c] = (abs( dvar[c] - dvar[c-1] )/ max( abs(dvar[c]) + abs(dvar[c-1]), 0.0000000001))^2
-    end
-
-    return sj
-end
-
-function structured_1D_JST_rj(T::Array{Float64, 1}, U::Array{Float64, 1}, gamma=1.4, R=287.05)
-    nCells = size(T, 1)
-    rj = Array{Float64, 1}(undef, nCells)
-
-    for c in 1:nCells
-        rj[c] = abs(U[c]) + sqrt(gamma * R * T[c])
-    end
-    return rj
-end
-
-function structured_1D_JST_Eps(dx, k2, k4, c4, cellPrimitives::Array{Float64,2}, gamma=1.4, R=287.05)
-    nFaces = size(cellPrimitives, 1) + 1
-
-    # Pressure sensor seems to work best for the shock tube
-    dP = structured_1DFaceDelta(dx, cellPrimitives[:,1])[1]
-    sj = structured_1D_JST_sj2(dP)
-
-    rj = structured_1D_JST_rj(cellPrimitives[:,2], cellPrimitives[:,3], gamma, R)
-    sjF, rjF = structured_1DMaxInterp(dx, sj, rj)
-
-    eps2 = Array{Float64, 1}(undef, nFaces)
-    eps4 = Array{Float64, 1}(undef, nFaces)
-    for f in 2:nFaces-1
-        eps2[f] = k2 * sjF[f] * rjF[f]
-        eps4[f] = max(0, k4*rjF[f] - c4*eps2[f])
-    end
-
-    return eps2, eps4
-end
-
-# Requires correct cellState and cellPrimitives as input
-function structured_JSTFlux(dx, solutionState)
-    cellState, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes = solutionState
-    nCells = size(cellState, 1)
-    nFaces = nCells + 1
-    faceDeltas = Array{Float64, 2}(undef, nFaces, 3)
-
-    # Centrally differenced fluxes
-    structured_1DlinInterp(dx, faceFluxes, cellFluxes)
-
-    #### Add JST artificial Diffusion ####
-    structured_1DFaceDelta(dx, faceDeltas, cellState)
-    eps2, eps4 = structured_1D_JST_Eps(dx, 0.5, (1/32), 0, cellPrimitives)
-    # nCells = nFaces - 1
-    for f in 2:(nCells)
-        for v in 1:3
-            diffusionFlux = eps2[f]*faceDeltas[f, v] - eps4[f]*(faceDeltas[f+1, v] - 2*faceDeltas[f, v] + faceDeltas[f-1, v])
-            faceFluxes[f,v] -= diffusionFlux
-        end
-    end
-
-    return integrateFluxes_structured(dx, solutionState)
 end
 
 # Requires correct cellState and cellPrimitives as input
@@ -555,7 +335,7 @@ function unstructured_JSTFlux(mesh, solutionState)
         end
     end
 
-    return integrateFluxes_structured(dx, solutionState)
+    return integrateFluxes_structured1D(dx, solutionState)
 end
 
 ######################### TimeStepping #######################
@@ -569,7 +349,7 @@ function decodeSolution(solutionState)
     end
 end
 
-function integrateFluxes_structured(dx, solutionState)
+function integrateFluxes_structured1D(dx, solutionState)
     cellState, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes = solutionState
     nCells = size(cellState, 1)
 
@@ -601,7 +381,7 @@ function integrateFluxes_structured(dx, solutionState)
     return fluxResiduals
 end
 
-function integrateFluxes_unstructured(mesh, solutionState)
+function integrateFluxes_unstructured3D(mesh, solutionState)
     cells, cVols, cCenters, faces, fAVecs, fCenters, boundaryFaces = mesh
     cellState, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes = solutionState
     nCells = size(cells, 1)
@@ -611,11 +391,18 @@ function integrateFluxes_unstructured(mesh, solutionState)
     # Recomputing flux balances, so wipe existing values
     fill!(fluxResiduals, 0)
 
+    # TODO: Boundary Treatment HERE
+
     # Face-based integration of fluxes
-    #TODO: Multiply by face area here
     for f in 2:nFaces-1
         for v in 1:nVars
-            flux = faceFluxes[f,v]
+            flux = 0
+            for d in 1:3
+                # Dot product
+                var = (v-1)*3 + (d+1)
+                flux += faceFluxes[f,var]*fAVecs[f, d]
+            end
+
             ownerCell = faces[f][1]
             neighbourCell = faces[f][2]
             # Subtract from owner cell
@@ -630,80 +417,7 @@ function integrateFluxes_unstructured(mesh, solutionState)
         fluxResiduals[c,:] ./= cVols[c]
     end
 
-    # TODO: Boundary Treatment HERE
-
     return fluxResiduals
-end
-
-function forwardEuler(mesh, fluxResidualFn, solutionState, dt)
-    cellState, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes = solutionState
-
-    fluxResiduals = fluxResidualFn(mesh, solutionState)
-    cellState .+= fluxResiduals.*dt
-    decodeSolution(solutionState)
-
-    return solutionState
-end
-
-function RK2_Mid(mesh, fluxResidualFn, solutionState, dt)
-    cellState, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes = solutionState
-
-    fluxResiduals1 = fluxResidualFn(mesh, solutionState)
-    halfwayEstimate = cellState .+ fluxResiduals1.*dt/2
-    solutionState2 = [ halfwayEstimate, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes ]
-    decodeSolution(solutionState2)
-
-    fluxResiduals2 = fluxResidualFn(mesh, solutionState2)
-    cellState .+= fluxResiduals2.*dt
-    decodeSolution(solutionState)
-
-    return solutionState
-end
-
-function RK4(mesh, fluxResidualFn, solutionState, dt)
-    cellState, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes = solutionState
-
-    fluxResiduals1 = fluxResidualFn(mesh, solutionState)
-    halfwayEstimate = cellState .+ fluxResiduals1*dt/2
-    lastSolutionState = [ halfwayEstimate, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes ]
-    decodeSolution(lastSolutionState)
-
-    fluxResiduals2 = fluxResidualFn(mesh, lastSolutionState)
-    halfwayEstimate2 = cellState .+ fluxResiduals2*dt/2
-    lastSolutionState = [ halfwayEstimate2, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes ]
-    decodeSolution(lastSolutionState)
-
-    fluxResiduals3 = fluxResidualFn(mesh, lastSolutionState)
-    finalEstimate1 = cellState .+ fluxResiduals3*dt
-    lastSolutionState = [ finalEstimate1, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes ]
-    decodeSolution(lastSolutionState)
-
-    fluxResiduals4 = fluxResidualFn(mesh, lastSolutionState)
-    cellState .+= (fluxResiduals1 .+ 2*fluxResiduals2 .+ 2*fluxResiduals3 .+ fluxResiduals4 )*(dt/6)
-    decodeSolution(solutionState)
-
-    return solutionState
-end
-
-function ShuOsher(mesh, fluxResidualFn, solutionState, dt)
-    cellState, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes = solutionState
-
-    fluxResiduals1 = fluxResidualFn(mesh, solutionState)
-    endEstimate = cellState .+ fluxResiduals1.*dt
-    lastSolutionState = [ endEstimate, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes ]
-    decodeSolution(lastSolutionState)
-
-    fluxResiduals2 = fluxResidualFn(mesh, lastSolutionState)
-    estimate2 = (3/4).*cellState .+ (1/4).*(endEstimate .+ fluxResiduals2.*dt)
-    lastSolutionState = [ estimate2, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes ]
-    decodeSolution(lastSolutionState)
-
-    fluxResiduals3 = fluxResidualFn(mesh, lastSolutionState)
-    cellState .= (1/3).*cellState .+ (2/3).*(estimate2 .+ dt.*fluxResiduals3)
-    solutionState = [ cellState, cellFluxes, cellPrimitives, fluxResiduals, faceFluxes ]
-    decodeSolution(solutionState)
-
-    return solutionState
 end
 
 ######################### Solvers #######################
@@ -831,7 +545,7 @@ function central_UnstructuredADFVM(mesh, P, T, U; initDt=0.001, endTime=0.14267,
     return P, U, T, rho
 end
 
-function structured1DFVM(dx::Array{Float64, 1}, cellPrimitives::Array{Float64, 2}, timeIntegrationFn=forwardEuler, fluxFunction=structured_JSTFlux; initDt=0.001, endTime=0.14267, targetCFL=0.2, gamma=1.4, R=287.05, Cp=1005, silent=true)
+function structured1DFVM(dx::Array{Float64, 1}, cellPrimitives::Array{Float64, 2}, timeIntegrationFn=forwardEuler, fluxFunction=structured_JSTFlux1D; initDt=0.001, endTime=0.14267, targetCFL=0.2, gamma=1.4, R=287.05, Cp=1005, silent=true)
     if !silent
         println("Initializing Simulation")
     end
