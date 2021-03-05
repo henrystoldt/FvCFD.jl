6d7851709c6bd317b8f74efa292df68c160a7909
henrystoldt <henrystoldt@users.noreply.github.com>
2021-03-04 22:48:13 -0700

    Rename Test->test in runTests.jl for compatibility with the previous commit

--- test/runTests.jl
+++ test/runTests.jl
@@ -1,5 +1,5 @@
 # Runs tests from any file which which starts with "test_" and ends with ".jl"
-files = readdir("./Test")
+files = readdir("./test")
 
 for file in files
     if lastindex(file) > 5 && file[1:5] == "test_" && file[end-2:end] == ".jl"
