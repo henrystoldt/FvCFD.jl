22bb315013de202f289788506c2725fd27e37669
henrystoldt <henrystoldt@users.noreply.github.com>
2021-03-06 10:03:11 -0700

    runtests.jl will now work when run by Pkg.test or by a simply including the file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 # Runs tests from any file which which starts with "test_" and ends with ".jl"
-files = readdir("./test")
 
+# First assume the file is running from ./test (this occurs when running Pkg.test)
+files = readdir(".")
+pathPrefix = ""
+
+# Check if we are actually running from the main directory, and read files from ./test instead
+    # this occurs when running include("runtests.jl") from a julia instance in the main directory
+if "src" in files
+    files = readdir("./test")
+    pathPrefix = "./test/"
+end
+
+# Look for and run test files
 for file in files
     if lastindex(file) > 5 && file[1:5] == "test_" && file[end-2:end] == ".jl"
         println("Running Tests from: test/$file")
