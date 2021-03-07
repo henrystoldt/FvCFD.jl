c20bb313592b055999a707369de5e17634f1bf70
henrystoldt <henrystoldt@users.noreply.github.com>
2021-03-07 12:07:12 -0700

    3D Cone Flare Example

---
+++ Examples/3DConeFlare.jl
@@ -0,0 +1,18 @@
+using fvCFD
+
+# All file paths are relative to the repository's main directory, 'include' this script from there
+
+### Freestream Conditions ###
+P = 100000
+T = 300
+U = [ 694.26, 0, 0 ]
+Cp = 1005
+
+### Choose a mesh ###
+meshPath = "test/OFflaredConeMesh"
+boundaryConditions = [ supersonicInletBoundary, [P, T, U..., Cp], zeroGradientBoundary, [], wallBoundary, [] ]
+
+### Solve ###
+mesh = OpenFOAMMesh(meshPath)
+cellPrimitives = initializeUniformSolution3D(mesh, P, T, U...)
+solve(mesh, meshPath, cellPrimitives, boundaryConditions, LTSEuler, initDt=0.00000001, endTime=18000, outputInterval=9.3, targetCFL=0.1, silent=false, restart=false)
--- Examples/runSolver.jl
+++ Examples/runSolver.jl
@@ -1,32 +1,4 @@
 include("fvCFD.jl")
-include("shockTube.jl")
-
-###################################################### FORMERLY RUNAIRFOIL ###############################################################
-# Freestream Conditions
-# P = 100000
-# T = 300
-# U = [ 277.7091, 6.059633, 0 ]
-# UunitVec = normalize(U)
-# gamma = 1.4
-# R = 287.05
-# Cp = 1005
-
-# a = sqrt(gamma*R*T)
-# machNum = mag(U)/a
-# Pt = P*(1 + ((gamma-1)/2)*machNum^2)^(gamma/(gamma-1))
-# Tt = T*(1 + ((gamma-1)/2)*machNum^2)
-
-# Choose a mesh
-# meshPath = "../test/OFairfoilMesh"
-# meshPath = "../test/OFmemesAirfoilMesh"
-# boundaryConditions = [ wallBoundary, [], emptyBoundary, [], subsonicInletBoundary, [Pt, Tt, UunitVec..., gamma, R, Cp], pressureOutletBoundary, P ]
-#
-# Solve
-# mesh = OpenFOAMMesh(meshPath)
-# cellPrimitives = initializeUniformSolution3D(mesh, P, T, U...)
-# solve(mesh, meshPath, cellPrimitives, boundaryConditions, LTSEuler, initDt=0.0000001, endTime=100, outputInterval=25, targetCFL=0.5, silent=false)
-# solve(mesh, meshPath, cellPrimitives, boundaryConditions, initDt=0.0000001, endTime=0.00008, outputInterval=0.00002, targetCFL=0.5, silent=false)
-
 
 # ### Delta Wing ###
 # # Freestream Conditions
@@ -37,18 +9,3 @@ include("shockTube.jl")
 # # Choose a mesh
 # meshPath = "../test/OFmemesDeltaMesh"
 # boundaryConditions = [ supersonicInletBoundary, [P, T, U...], zeroGradientBoundary, [], symmetryBoundary, [], wallBoundary, [] ]
-
-### Cone Flare ###
-# Freestream Conditions
-# P = 100000
-# T = 300
-# U = [ 694.26, 0, 0 ]
-#
-# # Choose a mesh
-# meshPath = ../test/OFflaredConeMesh"
-# boundaryConditions = [ supersonicInletBoundary, [P, T, U...], zeroGradientBoundary, [], wallBoundary, [] ]
-
-# Solve
-# mesh = OpenFOAMMesh(meshPath)
-# cellPrimitives = initializeUniformSolution3D(mesh, P, T, U...)
-# solve(mesh, meshPath, cellPrimitives, boundaryConditions, LTSEuler, initDt=0.00000001, endTime=18000, outputInterval=9.3, targetCFL=0.1, silent=false, restart=false)
