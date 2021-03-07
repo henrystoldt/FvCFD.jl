b82f3e5232200e040c809c413703111d1ea0e703
henrystoldt <henrystoldt@users.noreply.github.com>
2021-03-07 10:25:20 -0700

    Forward step example working

---
+++ Examples/forwardStep.jl
@@ -0,0 +1,19 @@
+using fvCFD
+
+# All file paths are relative to the repository's main directory, 'include' this script from there
+
+### Freestream Conditions (Mach 3) ###
+P = 100000
+T = 300
+U = [ 1041.66, 0, 0 ]
+
+### Choose a mesh ###
+# meshPath = "test/OFforwardStepMesh"
+# boundaryConditions = [ supersonicInletBoundary, [P, T, U..., 1005], zeroGradientBoundary, [], symmetryBoundary, [], symmetryBoundary, [], wallBoundary, [], emptyBoundary, [] ]
+meshPath = "test/OFforwardStepFineMesh"
+boundaryConditions = [ supersonicInletBoundary, [P, T, U..., 1005], zeroGradientBoundary, [], symmetryBoundary, [], symmetryBoundary, [], wallBoundary, [], emptyBoundary, [] ]
+
+### Run ###
+mesh = OpenFOAMMesh(meshPath)
+cellPrimitives = initializeUniformSolution3D(mesh, P, T, U...)
+solve(mesh, meshPath, cellPrimitives, boundaryConditions, ShuOsher, initDt=0.00000001, endTime=0.03, outputInterval=0.000072, targetCFL=0.95, silent=false, restart=false)
